2dd65a4cbd3ccc1e91cd703112265451f773b85a
Avinash Kumar <105458143+avinashDeveloper@users.noreply.github.com>
2024-01-23 17:19:31 +0530

    Add files via upload

---
+++ Foodie Hamburger Project/script.js
@@ -0,0 +1,22 @@
+const hamburgerContainer = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
+
+hamburgerContainer.addEventListener('click', (e) => {
+    e.stopPropagation()
+    headerContent.classList.add('menu-open')
+})
+
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+
+window.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
